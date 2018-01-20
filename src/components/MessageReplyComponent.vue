c36fcfc73d3d7219e1bfa99ea35d46a9f9bede19
awvaarua <ganstersb2@gmail.com>
2018-01-20 19:44:45 +0100

    Add image suport and full page option

--- src/components/MessageReplyComponent.vue
+++ src/components/MessageReplyComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <tinymce id="d1" v-model="body"></tinymce>
+    <tinymce id="replyTiny" :other_options="options" :toolbar1="toolbar1" v-model="bodyCopy"></tinymce>
   </div>
 </template>
 
@@ -12,11 +12,16 @@ export default {
   components: { tinymce },
   data () {
     return {
-      config: {
+      bodyCopy: this.body.repeat(1),
+      plugins: ['fullscreen'],
+      toolbar1: 'formatselect | bold italic  strikethrough  forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat | fullscreen',
+      options: {
+        paste_data_images: true,
+        images_upload_handler: function (blobInfo, success, failure) {
+          success(`data:${blobInfo.blob().type};base64,${blobInfo.base64()}`)
+        }
       }
     }
-  },
-  mounted () {
   },
   props: {
     body: {
